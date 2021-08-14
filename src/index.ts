0a128dfc4e07f41cd69241ae39e0c62774030869
t-bendet <talbendet21@gmail.com>
2021-08-14 13:49:37 +0300

    first commit

---
+++ src/index.ts
@@ -0,0 +1,17 @@
+import express from "express";
+import bodyParser from "body-parser";
+import cookieSession from "cookie-session";
+import { AppRouter } from "./AppRouter";
+import "./controllers/LoginController";
+import "./controllers/RootController";
+
+const app = express();
+
+app.use(bodyParser.urlencoded({ extended: true }));
+// app.use(express.json());
+// attach a session property to the request object
+app.use(cookieSession({ keys: ["laskdjf"] }));
+app.use(AppRouter.getInstance());
+app.listen(3000, () => {
+  console.log("Listening on port 3000");
+});
